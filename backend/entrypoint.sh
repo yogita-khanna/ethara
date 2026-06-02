90e02c6e6285c9c808f4fbbadafde3da220374b8
yogita-khanna <yogitakhanna06@gmail.com>
2026-06-02 16:26:10 +0530

    Fix local PORT issue in entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -8,4 +8,4 @@ alembic -c /app/alembic.ini upgrade head
 python seed.py
 
 # Start the FastAPI server
-exec uvicorn app.main:app --host 0.0.0.0 --port $PORT
+exec uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
